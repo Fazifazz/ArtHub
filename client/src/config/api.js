ecf165c79efdf6c78a2379285666685f7f172f39
Fazifazz <fasilpambalath7@gmail.com>
2024-01-30 15:53:31 +0530

    apibaseurl changed with render url

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,4 +1,4 @@
 // src/config/api.js
-export const API_BASE_URL = 'http://localhost:5000';
-// export  const API_BASE_URL = 'https://arthub-m0og.onrender.com';
+// export const API_BASE_URL = 'http://localhost:5000';
+export  const API_BASE_URL = 'https://arthub-m0og.onrender.com';
 
